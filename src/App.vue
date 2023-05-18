9cade01cbf0e2cb88c64e514fa425f8ab493d6c5
efrainwsr <efrain.figueredowsr@gmail.com>
2023-05-18 01:22:46 -0400

    Tabla cargada

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+//mport Table from './components/Table.vue'
+import Dtable from './components/Dtable.vue'
+
+
+</script>
+
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-2"></div>
+      <div class="col-8">
+        <Dtable/>
+      </div>
+      <div class="col-2"></div>
+    </div>
+    
+  </div>
+</template>
---
+++ src/components/Dtable.vue
@@ -0,0 +1,45 @@
+<script setup>
+import DataTable from 'datatables.net-vue3';
+import DataTablesCore from 'datatables.net-bs5';
+ 
+DataTable.use(DataTablesCore);
+ 
+const options = {
+  responsive: true,
+  select: true,
+  language:{
+    "emptyTable":     "No hay datos disponibles",
+    "info":           "Mostrando _START_ a _END_ de _TOTAL_ entradas",
+    "infoEmpty":      "Mostrando 0 to 0 de 0 entradas",
+    "infoFiltered":   "(Filtrado de _MAX_ entradas totales)",
+    "lengthMenu":     "Mostrando _MENU_ Resultados",
+    "loadingRecords": "Cargando...",
+    "search":         "Buscar:",
+    "zeroRecords":    "No matching records found",
+    "paginate": {
+        "first":      "Primero",
+        "last":       "Ultimo",
+        "next":       "Siguiente",
+        "previous":   "Anterior"
+    },
+}
+}
+
+const data = [
+  [1, 2],
+  [3, 4],
+];
+</script>
+ 
+<template>
+    <DataTable class="display table table-hover table-dark"
+    :data="data" 
+    :options="options">
+        <thead>
+            <tr>
+                <th>A</th>
+                <th>B</th>
+            </tr>
+        </thead>
+    </DataTable>
+</template>
