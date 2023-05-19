cd5fb9113d02fbb89c90d9b33a2de1827794b91e
efrain <efrain.akl@gmail.com>
2023-05-19 14:41:00 -0400

    Muestra las columnas una sobre otra

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,35 @@
 <script setup>
-
+// https://morioh.com/p/a0fe12e6daa0
 //mport Table from './components/Table.vue'
+import { ref } from 'vue'
 import Dtable from './components/Dtable.vue'
 import Clientes from './views/Clientes.vue'
 
+const pok = {
+	name: '',
+	weight: '',
+	height: ''
+}
+
+
+const fetchPokemon = async ()=>{  
+	const res  = await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+	const data = await res.json()
+	
+	pok.name = data.name
+	pok.weight = data.weight
+	pok.height = data.height
+ }
+ 
+ const colum = ['Nombre', 'Tamaño', 'Peso']
+ 
+ const data = [
+       {name: "Ditto", tamano:"Computer Science", peso:"50"},
+      {name: "Pikachu", tamano:"Philosophy", peso:"20"},  
+     ]
+
+fetchPokemon()
+console.log(pok)
 
 </script>
 
@@ -12,10 +38,13 @@ import Clientes from './views/Clientes.vue'
     <div class="row">
       <div class="col-2"></div>
       <div class="col-8">
-        <Clientes/>
+        <Dtable  :cols="colum" :data="data"/>
       </div>
       <div class="col-2"></div>
     </div>
     
   </div>
 </template>
+
+
+
--- src/components/Dtable.vue
+++ src/components/Dtable.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted, watch } from 'vue';
+import { ref, onMounted, watch, defineProps } from 'vue';
 import DataTable from 'datatables.net-vue3';
 import DataTablesBs5 from 'datatables.net-bs5';
 
@@ -7,10 +7,12 @@ import DataTablesBs5 from 'datatables.net-bs5';
 DataTable.use(DataTablesBs5);
 
 
-const tableRef = ref(null);
-
 const props = defineProps({
-  data: {
+	cols:{ type: Array, required: true},
+	data:{type: Array, required: true}
+	
+
+  /*data: {
     type: Array,
     required: true,
   },
@@ -18,24 +20,50 @@ const props = defineProps({
     type: Object,
     default: () => {},
   },
+  cols: {
+	  type: Array,
+	  required: true,
+  }
+  */
 
   
 });
 
-onMounted(() => {
-  const table = DataTable(tableRef.value, {
-    data: props.data,
-  });
-
-  // manejar cambios en los datos
-  watch(() => props.data, () => {
-    table.clear().rows.add(props.data).draw();
-  });
-});
+ 
+	const options = {
+	
+	    language:{
+	      "emptyTable":     "No hay datos disponibles",
+	      "info":           "Mostrando _START_ a _END_ de _TOTAL_ entradas",
+	      "infoEmpty":      "Mostrando 0 to 0 de 0 entradas",
+	      "infoFiltered":   "(Filtrado de _MAX_ entradas totales)",
+	      "lengthMenu":     "Mostrando _MENU_ Resultados",
+	      "loadingRecords": "Cargando...",
+	      "search":         "Buscar:",
+	      "zeroRecords":    "No matching records found",
+	      "paginate": {
+	          "first":      "Primero",
+	          "last":       "Ultimo",
+	          "next":       "Siguiente",
+	          "previous":   "Anterior"
+	      },
+	    }
+	  }
 
 </script>
 
 <template>
-  <DataTable :ref="tableRef" />
+  <DataTable id="myTable" :options="options" class="display table table-hover">
+      <thead>
+          <tr v-for="column in cols">
+              <th>{{ column }}</th>
+          </tr>
+      </thead>
+      <tbody>
+            <tr v-for="item in data" :key='item'>
+            <td v-for="column in cols" :key='column'>{{item[column]}}</td>
+          </tr>
+      </tbody>
+  </DataTable>
 </template>
   
